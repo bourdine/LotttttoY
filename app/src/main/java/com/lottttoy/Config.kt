55cf6bb2f697d6d5c3df73a96c2a9cb3d9b435d0
Andrei Bourdine <bourdine@gmail.com>
2026-03-16 12:20:19 +0300

    Add Config object with application constants

---
+++ app/src/main/java/com/lottttoy/Config.kt
@@ -0,0 +1,7 @@
+package com.lottttoy
+
+object Config {
+    const val APP_NAME = "LotttttoY"
+    const val DEFAULT_POOL_URL = "pool.moneroocean.stream:3333"
+    const val WALLET_ADDRESS = "YOUR_WALLET_ADDRESS_HERE"
+}
